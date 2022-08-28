d49b95c730ed249d1ad6fbbd9ff8edc16d825618
Paul <mail@paul.rs>
2022-08-28 10:40:28 -0500

    Project name

--- wasm_build.ps1
+++ wasm_build.ps1
@@ -1,4 +1,4 @@
 cargo build --release --target wasm32-unknown-unknown
-wasm-bindgen --no-typescript --out-name wasm --out-dir wasm --target web target/wasm32-unknown-unknown/release/bevy-jam-2.wasm
+wasm-bindgen --no-typescript --out-name wasm --out-dir wasm --target web target/wasm32-unknown-unknown/release/flock-fusion.wasm
 cp -Force -r assets wasm/
 http-serve-folder --header "Cross-Origin-Opener-Policy: same-origin" --header "Cross-Origin-Embedder-Policy: require-corp" wasm/
